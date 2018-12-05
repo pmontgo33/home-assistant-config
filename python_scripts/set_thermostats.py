b36bbbd35bdc6bdb20de59cf049e315cc9da3e04
Patrick W. Montgomery <pmontgo33@gmail.com>
2018-12-05 07:47:02 -0500

    Switch to Lovelave UI

--- python_scripts/set_thermostats.py
+++ python_scripts/set_thermostats.py
@@ -16,36 +16,41 @@ if heating:
 
 house_mode = hass.states.get('sensor.house_mode').state
 logger.warning("Current House Mode is: {}".format(house_mode))
-operation_mode = hass.states.get('climate.living_room').attributes['operation_mode']
+operation_mode = hass.states.get('input_select.home_climate_mode').state
 logger.warning("Current Operation Mode: {}".format(operation_mode))
 
 if house_mode == 'Away':
   dstairs_heat_setpoint = 68
-  upstairs_radiant_heat_setpoint = 61
+  upstairs_radiant_heat_setpoint = 65
   
   dstairs_cool_setpoint = 84
   
 elif house_mode == 'Sleep':
+  # hass.services.call('climate', 'set_away_mode', {'entity_id': 'climate.downstairs', 'away_mode': 'off'})
+  
   dstairs_heat_setpoint = 68
-  upstairs_radiant_heat_setpoint = 61
+  upstairs_radiant_heat_setpoint = 65
   
   dstairs_cool_setpoint = 84
   
 elif house_mode == 'Home':
-  dstairs_heat_setpoint = 70
-  upstairs_radiant_heat_setpoint = 69
+  # hass.services.call('climate', 'set_away_mode', {'entity_id': 'climate.downstairs', 'away_mode': 'off'})
   
-  dstairs_cool_setpoint = 79
+  dstairs_heat_setpoint = 70
+  upstairs_radiant_heat_setpoint = 70
+  
+  dstairs_cool_setpoint = 75
 
-if operation_mode == 'heat':
-  hass.services.call('climate', 'set_temperature', {'entity_id': 'climate.living_room', 'temperature': dstairs_heat_setpoint})
-  hass.services.call('climate', 'set_temperature', {'entity_id': 'climate.dining_room', 'temperature': dstairs_heat_setpoint})
+if operation_mode == 'Heat':
+  # hass.services.call('climate', 'set_temperature', {'entity_id': 'climate.downstairs', 'temperature': dstairs_heat_setpoint})
+  # hass.services.call('climate', 'set_temperature', {'entity_id': 'climate.dining_room', 'temperature': dstairs_heat_setpoint})
+  hass.services.call('climate', 'set_temperature', {'entity_id': 'group.downstairs_climate', 'temperature': dstairs_heat_setpoint})
   logger.warning("Downstairs Heat Setpoint: {}".format(dstairs_heat_setpoint))
   
   hass.services.call('climate', 'set_temperature', {'entity_id': 'climate.master_bath', 'temperature': upstairs_radiant_heat_setpoint})
   hass.services.call('climate', 'set_temperature', {'entity_id': 'climate.hall_bath', 'temperature': upstairs_radiant_heat_setpoint})
   logger.warning("Upstairs Radiant Heat Setpoint: {}".format(upstairs_radiant_heat_setpoint))
   
-elif operation_mode == 'cool':
-  hass.services.call('climate', 'set_temperature', {'entity_id': 'climate.living_room', 'temperature': dstairs_cool_setpoint})
-  logger.warning("Downstairs Cool Setpoint: {}".format(dstairs_cool_setpoint))
+# elif operation_mode == 'cool':
+#   hass.services.call('climate', 'set_temperature', {'entity_id': 'climate.living_room', 'temperature': dstairs_cool_setpoint})
+#   logger.warning("Downstairs Cool Setpoint: {}".format(dstairs_cool_setpoint))
